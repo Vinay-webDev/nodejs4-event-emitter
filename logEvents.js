a7c78871edc7ed2e51ecebfbb7eb7e72b5462f4b
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-09-03 01:10:14 +0530

    1. insatlling production dependencies and devDependencies

---
+++ logEvents.js
@@ -0,0 +1,97 @@
+//////////////////////////////////////////////////////////////////
+// event Emitter
+//👉👉👉remember before we add or install package to our project we need to initialize npm first👈👈👈
+/* install production dependencies ==>> npm i date-fns
+                                        npm i uuid
+and install nodemon as devDependency ===>> npm i nodemon -D
+*/
+///////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////////
+/*PS F:\Documents\github\nodejs4-event-emitter> npm init -y
+Wrote to F:\Documents\github\nodejs4-event-emitter\package.json:
+
+{
+  "name": "nodejs4-event-emitter",
+  "version": "1.0.0",
+  "description": "",
+  "main": "logEvents.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC"
+}
+
+
+PS F:\Documents\github\nodejs4-event-emitter> npm i date-fns
+
+added 1 package, and audited 2 packages in 18s
+
+1 package is looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS F:\Documents\github\nodejs4-event-emitter> npm i uuid
+
+added 1 package, and audited 3 packages in 2s
+
+2 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS F:\Documents\github\nodejs4-event-emitter> npm i nodemon -D
+
+added 29 packages, and audited 32 packages in 2s
+
+6 packages are looking for funding
+  run `npm fund` for details
+
+found 0 vulnerabilities
+PS F:\Documents\github\nodejs4-event-emitter> 
+*/
+/////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
