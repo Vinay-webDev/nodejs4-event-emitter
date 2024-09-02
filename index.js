8a6c70c3ab2e03cae7dfa453c582d6e3150550df
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-09-03 01:45:43 +0530

    5. myEmitter.emit( )
    this is confusing so practice more!

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 //////////////////////////////////////////////////
+// this is little confusing so practice more!
 // import logEvents from ./logEvents.js
 const logEvents = require('./logEvents');
 // we also need to import the 'events' common core module 
-const eventEmitter = require('events');
+const EventEmitter = require('events');
 
+//according to the documentation
+//we need to define a class 
+class MyEmitter extends EventEmitter {};
 
+//initialize the object or instance👇
+const myEmitter = new MyEmitter();
 
+// we need to add event listener for the log event
+myEmitter.on('log', (msg) => {
+    logEvents(msg);
+} );
 
-
-
-
+//setTimeout to have delayed log events
+setTimeout(() => {
+    myEmitter.emit('log', 'Event log emitted!');
+}, 2000);
+/* as we can see we got our logData logged to the console
+however we got into an error if you observe there's no directory called 'logs' in the file tree
+the appendFile just create a new file but can't able to create a directory or folder for us */
 
 
 
