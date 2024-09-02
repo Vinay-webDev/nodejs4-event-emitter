f2017df55162f787fca71c331e5314c0fe31d67d
ninjaprogrammer2999 <149578035+ninjaprogrammer2999@users.noreply.github.com>
2024-09-03 01:30:44 +0530

    4. import logEvents and events
    logEvents is custom module
    events is a common core module

---
+++ index.js
@@ -0,0 +1,24 @@
+//////////////////////////////////////////////////
+// import logEvents from ./logEvents.js
+const logEvents = require('./logEvents');
+// we also need to import the 'events' common core module 
+const eventEmitter = require('events');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
